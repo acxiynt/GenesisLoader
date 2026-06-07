4dca78ea1d328313a36bc248484a3638e5c7b04a
acxiynt <3059872@gmail.com>
2026-06-07 12:35:44 +0900

    i cannot rename the namespace and entrypoint lmao

---
+++ GenesisLoaderStandalone/Bootstrap.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Genesis;
+namespace Doorstop
+{
+    public class Entrypoint
+    {
+        public static bool UnityStarted = false;
+        private static void OnGameInit()
+        {
+            if (!UnityStarted)
+            {
+                Application.logMessageReceivedThreaded += Util.LogUnity;
+                UnityStarted = true;
+                Util.LogString("Game initialized");
+                Main.OnGameInit().Wait();
+                Util.LogString("Started initializing mod and plugins");
+            }
+        }
+        private static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (!UnityStarted)
+            {
+                OnGameInit();
+                UnityStarted = true;
+            }
+            _ = Main.OnSceneLoaded();
+        }
+
+        public static void Start()
+        {
+            Main.EnsureDirectory();
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            Config.ReadFile("./GenesisLoader.cfg");
+            Util.LogString("Preinitalization started.");
+            List<Task> tasks = [Main.LoadMod(), Main.PreInit()];
+            Task.WhenAll(tasks).Wait();
+            Main.Init().Wait();
+            Util.LogString("Preinitalization finished.");
+        }
+    }
+}
